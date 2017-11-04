2484b224a9cab882bc14769e3e5d58f13f0aab9e
kongmile <943643887@qq.com>
2017-11-04 21:31:56 +0800

    注册登陆接口

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-$api = app('Dingo\Api\Routing\Router');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -20,8 +19,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 //Route::resource('lesson', 'LessonController');
 
+$api = app('Dingo\Api\Routing\Router');
+
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
-        $api->get('lesson', 'LessonController@index');
+        $api->post('user/login', 'AuthController@authenticate');
+        $api->post('user/register', 'AuthController@register');
+        $api->group(['middleware' => 'jwt.auth'], function ($api) {
+            $api->get('user/me','AuthController@getAuthenticatedUser');
+            $api->get('lesson', 'LessonController@index');
+            $api->get('lesson/{id}','LessonController@show');
+        });
+
     });
 });
