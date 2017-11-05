0d7654406f5e6835714cd99ab75a477f1b62d32a
kongmile <943643887@qq.com>
2017-11-05 17:00:37 +0800

    修改登陆验证返回信息

--- app/Api/Controllers/AuthController.php
+++ app/Api/Controllers/AuthController.php
@@ -16,11 +16,11 @@ class AuthController extends BaseController
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
+                return $this->response->error('invalid_credentials', 401);
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
+            return $this->response->error('could_not_create_token', 500);
         }
 
         // all good so return the token
